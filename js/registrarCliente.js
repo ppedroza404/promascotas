edd90e5767f1306607445e9c84cc6f7fae275488
Roberto <rrojasj@ucenfotec.ac.cr>
2021-03-16 00:34:42 -0600

    Formulario Registro - Cliente

--- js/registrarCliente.js
+++ js/registrarCliente.js
@@ -1,3 +1,34 @@
-removestyle = () => {
-        document.getElementById(
-            "input-imagen").removeAttribute("style");
+'use strict';
+
+let lista_provincias = [{
+    "1": "San José",
+    "2": "Alajuela",
+    "3": "Cartago",
+    "4": "Heredia",
+    "5": "Guanacaste",
+    "6": "Puntarenas",
+    "7": "Limón"
+}]
+
+let lista_canton_sj = [{
+    "1": "Central",
+    "2": "Escazú",
+    "3": "Desamparados",
+    "4": "Puriscal",
+    "5": "Tarrazú",
+    "6": "Aserrí",
+    "7": "Mora",
+    "8": "Goicoechea",
+    "9": "Santa Ana",
+    "10": "Alajuelita",
+    "11": "Vázquez De Coronado",
+    "12": "Acosta",
+    "13": "Tibás",
+    "14": "Moravia",
+    "15": "Montes De Oca",
+    "16": "Turrubares",
+    "17": "Dota",
+    "18": "Curridabat",
+    "19": "Pérez Zeledón",
+    "20": "León Cortés Castro"
+}]
