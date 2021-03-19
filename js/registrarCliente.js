4511d7262323f24564ac04cf1a3126fdfbf3008e
Roberto <rrojasj@ucenfotec.ac.cr>
2021-03-18 20:35:26 -0600

    1. Registrar Cliente - JS

--- js/registrarCliente.js
+++ js/registrarCliente.js
@@ -1,7 +1,31 @@
 'use strict';
 
-const btnRegistrarCliente = document.querySelector('#btn-registrarCliente');
+/* Función para habilitar o deshabilitar los campos de Cantón y Distrito */
 
+const provincia = document.querySelector('#slt-provincia');
+const canton = document.querySelector('#slt-canton');
+const distrito = document.querySelector('#slt-distrito');
+
+const habilitarCanton = () => {
+    if (provincia != '') {
+        canton.disabled = false;
+    } else {
+        canton.disabled = true;
+    }
+}
+const habilitarDistrito = () => {
+    if (canton != '') {
+        distrito.disabled = false;
+    } else {
+        distrito.disabled = true;
+    }
+
+}
+provincia.addEventListener('change', habilitarCanton);
+canton.addEventListener('change', habilitarDistrito);
+
+
+const btnRegistrarCliente = document.querySelector('#btn-registrarCliente');
 const inputNombreCliente = document.querySelector('#txt-nombreCliente');
 const inputPrimApellidoCliente = document.querySelector('#txt-primerApellidoCliente');
 const inputSegApellidoCliente = document.querySelector('#txt-segApellidoCliente');
@@ -16,165 +40,33 @@ const sltCanton = document.querySelector('#slt-canton');
 const sltDistrito = document.querySelector('#slt-distrito');
 const inputOtrasSenias = document.querySelector('#txt-otrasSenias');
 
-/* Inicio: Función para calcular la edad */
 
-const calcularEdad = (nacimiento) => {
-    let fechaActual = new Date();
-    let edad = fechaActual.getFullYear() - nacimiento.getFullYear();
+const validarEdad = (inputNacimiento) => {
+    const edadMin = 18;
+    let edad = calcularEdad(inputNacimiento)
 
-    if (fechaActual.getMonth() < nacimiento.getMonth()) {
-        edad = edad - 1;
-    } else {
-        if ((fechaActual.getMonth() == nacimiento.getMonth()) && (fechaActual.getUTCDate() < nacimiento.getUTCDate())) {
-            edad = edad - 1;
-        }
-    }
-
-    return edad;
-};
-/* Fin: Función para calcular la edad */
-
-
-/* Inicio: Función para validar los campos requeridos */
-
-const validar = () => {
-    let error = false;
-
-    let regexCorreo = /^[a-zA-Z.0-9]+\@{1}[a-zA-Z.]+$/;
-
-    if (inputNombreCliente.value == '') {
-        error = true;
-        inputNombreCliente.classList.add('error');
-    } else {
-        inputNombreCliente.classList.remove('error');
-    }
-
-    if (inputPrimApellidoCliente.value == '') {
-        error = true;
-        inputPrimApellidoCliente.classList.add('error');
-    } else {
-        inputPrimApellidoCliente.classList.remove('error');
-    }
-    if (inputSegApellidoCliente.value == '') {
-        error = true;
-        inputSegApellidoCliente.classList.add('error');
-    } else {
-        inputSegApellidoCliente.classList.remove('error');
-    }
-    if (sltGenero.value == '') {
-        error = true;
-        sltGenero.classList.add('error');
-    } else {
-        sltGenero.classList.remove('error');
-    }
-    if (sltTipoId.value == '') {
-        error = true;
-        sltTipoId.classList.add('error');
-    } else {
-        sltTipoId.classList.remove('error');
-    }
-    if (inputNumId.value == '') {
-        error = true;
-        inputNumId.classList.add('error');
-    } else {
-        inputNumId.classList.remove('error');
-    }
-    if (inputNacimiento.value == '') {
-        error = true;
-        inputNacimiento.classList.add('error');
-    } else {
-        inputNacimiento.classList.remove('error');
-    }
-    if (inputCorreo.value == '') {
-        error = true;
-        inputCorreo.classList.add('error');
-    } else {
-        inputCorreo.classList.remove('error');
-    }
-    if (inputCantMascotas.value == '') {
-        error = true;
-        inputCantMascotas.classList.add('error');
-    } else {
-        inputCantMascotas.classList.remove('error');
-    }
-    if (sltProvincia.value == '') {
-        error = true;
-        sltProvincia.classList.add('error');
-    } else {
-        sltProvincia.classList.remove('error');
-    }
-    if (sltCanton.value == '') {
-        error = true;
-        sltCanton.classList.add('error');
-    } else {
-        sltCanton.classList.remove('error');
-    }
-    if (sltDistrito.value == '') {
-        error = true;
-        sltDistrito.classList.add('error');
-    } else {
-        sltDistrito.classList.remove('error');
-    }
-
-    let edad = calcularEdad();
-
-    if (edad < 18) {
-        error = true;
-        inputNacimiento.classList.add('error');
-    } else {
-        inputNacimiento.classList.remove('error');
-    }
-
-    if (error == false) {
-        //obtenerDatos();
+    if (edad >= edadMin) {
+        validarInfo();
     } else {
         Swal.fire({
             'icon': 'warning',
-            'title': 'No se ha registrado',
-            'text': 'Por favor revise los campos resaltados'
+            'title': 'No es posible registrar un menor de edad',
+            'text': 'Muchas gracias por la comprensión',
+            'confirmButtonText': 'Entendido'
         });
     }
-}
+};
 
-/* Inicio: Función para validar los campos requeridos */
+const calcularEdad = (nacimiento) => {
+    let fechaActual = new Date();
+    let edad = fechaActual.getFullYear() - inputNacimiento.getFullYear();
 
-/* Inicio: Función de obtención de datos*/
-
-const obtenerDatos = () => {
-    let nombreCliente = inputNombreCliente.value;
-    let primerApellidoCliente = inputPrimApellidoCliente.value;
-    let segApellidoCliente = inputSegApellidoCliente.value;
-    let generoCliente = sltGenero.value;
-    let tipoId = sltTipoId.value;
-    let numId = inputNumId.value;
-    let fechaNacimiento = new Date(inputNacimiento.value);
-    let correo = inputCorreo.value;
-    let cantMascotas = inputCantMascotas.value;
-    let provincia = sltProvincia.value;
-    let canton = sltCanton.value;
-    let distrito = sltDistrito.value;
-    let otrasSenias = inputOtrasSenias.value;
-
-    let dia = Number(fechaNacimiento.getUTCDate());
-    let mes = Number(fechaNacimiento.getMonth() + 1);
-    let annio = Number(fechaNacimiento.getUTCFullYear());
-    let formatoFecha = `${dia}/${mes}/${annio}`;
-
-    registrarCliente(nombreCliente, primerApellidoCliente, segApellidoCliente, generoCliente, tipoId, numId, formatoFecha, correo, cantMascotas, provincia, canton, distrito, otrasSenias);
-
-    console.log(nombreCliente)
-}
-
-/* Fin: Función de obtención de datos*/
-
-/* Inicio: Función para habilitar selects de dirección */
-/*
-$('#slt-canton').click(function() {
-    if (sltProvincia != '') {
-        console.log(nombreCliente);
+    if (fechaActual.getMonth() < inputNacimiento.getMonth()) {
+        edad = edad - 1;
+    } else {
+        if ((fechaActual.getMonth() == inputNacimiento.getMonth()) && (fechaActual.getUTCDate() < inputNacimiento.getUTCDate())) {
+            edad = edad - 1;
+        }
     }
-})
-*/
-/* Inicio: Función para habilitar selects de dirección */
-
-btnRegistrarCliente.addEventListener('click', validar);
+    return edad;
+};
