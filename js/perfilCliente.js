8f4a1892e22b4d11cdf441549581c06c6059051e
Roberto <rrojasj@ucenfotec.ac.cr>
2021-03-21 01:17:46 -0600

    19. Perfil de proveedor - Cambios en la sección de denuncias

--- js/perfilCliente.js
+++ js/perfilCliente.js
@@ -5,10 +5,16 @@ const fotoMascota = document.querySelector('#imagen-prev');
 
 function mostrarInfo() {
     let infoMascota = document.getElementById('seccion-infoMascota');
+    let infoVacunas = document.getElementById('seccion-tablaMascotas');
     if (infoMascota.style.display === "none") {
         infoMascota.style.display = "block";
     } else {
         infoMascota.style.display = "none";
+    }
+    if (infoVacunas.style.display === "none") {
+        infoVacunas.style.display = "block";
+    } else {
+        infoVacunas.style.display = "none";
     }
 }
 
