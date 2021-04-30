aee66371bbecb0d9a5eaa36b847297d79e12af98
ppedroza404 <ppedrosas@ucenfotec.ac.cr>
2021-04-29 20:45:21 -0600

    update denunciados

--- js/listar-clientes-denunciados.js
+++ js/listar-clientes-denunciados.js
@@ -1,85 +1,63 @@
 'use strict';
+let estados = ["activo", "suspendido", "pendiente", "rechazado", "denunciado"];
 
 const tabla = document.querySelector('#tbl-solicitudespendientes tbody');
 const inputFiltro = document.querySelector('#txt-filtro');
+let lista_usuarios_cliente = [];
+let id_select;
+
+
+const crearSelect = () => {
+    const selectList = document.createElement("select");
+    selectList.classList.add('slt-estado');
+    selectList.setAttribute('id', `slt-${id_select}`);
+    selectList.disabled = true;
+
+    //Create and append the options
+    for (let i = 0; i < estados.length; i++) {
+        let option = document.createElement("option");
+        option.value = estados[i];
+        option.text = estados[i];
+        selectList.appendChild(option);
+    }
+    return selectList
+}
 
 const mostrarTabla = async() => {
     let filtro = inputFiltro.value.toLowerCase();
 
 
-    let lista_usuarios_cliente = [];
-
     lista_usuarios_cliente = await listar_usuarios_proveedor();
     tabla.innerHTML = '';
     lista_usuarios_cliente.forEach(cliente => {
 
-        if (cliente.denunciado.toLowerCase().includes('si')) {
+        if (cliente.estado.toLowerCase().includes('denunciado')) {
             if (cliente.estado.toLowerCase().includes(filtro) || cliente.provincia.toLowerCase().includes(filtro) || cliente.canton.toLowerCase().includes(filtro) || cliente.distrito.toLowerCase().includes(filtro)) {
                 let fila = tabla.insertRow();
-
+                id_select = cliente._id;
 
                 fila.insertCell().innerHTML = cliente.nombre + " " + cliente.primerApellido;
+                fila.insertCell().innerHTML = cliente.tipoUsuario;
                 fila.insertCell().innerHTML = cliente.correo;
                 fila.insertCell().innerHTML = cliente.provincia;
                 fila.insertCell().innerHTML = cliente.canton;
                 fila.insertCell().innerHTML = cliente.distrito;
-                fila.insertCell().innerHTML = cliente.denunciado;
+                let celda = fila.insertCell();
+                let select = crearSelect();
+                celda.appendChild(select).value = cliente.estado;
 
+                let testselect = document.querySelector(`#slt-${id_select}`);
 
-                /* let celdaAcciones = fila.insertCell();
+                const editarEstadosOnOff = () => {
+                    if (testselect.disabled == true) {
+                        testselect.disabled = false;
+                    } else {
+                        testselect.disabled = true;
+                    }
+                }
 
-                 let botonAprobar = document.createElement('button');
-                 botonAprobar.classList.add('btn');
-                 botonAprobar.innerText = 'Aprobar';
-                 botonAprobar.addEventListener('click', () => {*/
-                /*sessionStorage.setItem('usuarioSeleccionado', JSON.stringify(usuario));*/
-                /* botonAprobar.classList.add('btn-aprobar');
-                    botonModificar.classList.add('ocultar');
-                    botonAprobar.innerText = 'Aprobado';
-                });
+                celda.addEventListener('dblclick', editarEstadosOnOff);
 
-                celdaAcciones.appendChild(botonAprobar);
-
-                let celdaAcciones2 = fila.insertCell();
-
-                let botonModificar = document.createElement('button');
-                botonModificar.classList.add('btn');
-                botonModificar.innerText = 'Denegar';
-                botonModificar.addEventListener('click', () => {
-                    botonModificar.classList.add('btn-denegar');
-                    botonAprobar.classList.add('ocultar');
-                    botonModificar.innerText = 'Denegado';
-                });
-
-                celdaAcciones2.appendChild(botonModificar);*/
-
-
-                /* let botonEliminar = document.createElement('button');
-                botonEliminar.innerText = 'Eliminar';
-
-                botonEliminar.addEventListener('click', () => {
-                    Swal.fire({
-                        'icon': 'warning',
-                        'text': '¿Está seguro que desea borrar el usuario?',
-                        'showCancelButton': true,
-                        'confirmButtonText': '¡Sí!, estoy seguro',
-                        'cancelButtonColor': '#d33',
-                        'cancelButtonText': 'Cancelar',
-                        'reverseButtons': true
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            Swal.fire(
-                                '',
-                                'El usuario ha sido eliminado',
-                                'success'
-                            )
-                        }
-                    })
-                });
-                // Agregarle los botones a la celda
-
-                celdaAcciones.appendChild(botonModificar);
-                celdaAcciones.appendChild(botonEliminar);*/
             }
         }
     });
